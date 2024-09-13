c9c314f72b9f95f38d087e44094917a8214203c5
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-09-12 23:51:36 -0400

    Update api2.php
    Fixing Code to make the display more simple

--- api2.php
+++ api2.php
@@ -1,12 +1,12 @@
 <?php
-    $url = 'API URL Here';
+    $url = 'http://localhost:8006/api/v1/selection';
 
     $urlWithParams = $url;
     //initialize the url session
     $session = curl_init();
     //Set the session options 
     curl_setopt($session, CURLOPT_URL, $urlWithParams); //EndPoint
-    curl_setopt($session, CURLOPT_RETURNTRANSFER, $true); //Return a Response
+    //curl_setopt($session, CURLOPT_RETURNTRANSFER, $true); //Return a Response
 
     //Execute the request
     $response = curl_exec($session);
@@ -16,11 +16,10 @@
     } else {
         //Return JSON
         $responseData = json_decode($response, true);
-        //header('Content-Type: application/json');
+        header('Content-Type: application/json');
         echo json_encode($responseData);
     }
 
     //Close Session
     curl_close($session);
-
 ?>
